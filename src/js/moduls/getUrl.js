6e430710a7e7ec4f157401b893201aea4a5d18f7
VitAn1999 <elpistolero1999@gmail.com>
2021-07-12 15:18:24 +0300

    edit getUrl.js

--- src/js/moduls/getUrl.js
+++ src/js/moduls/getUrl.js
@@ -2,10 +2,12 @@ import { LANG_LIST } from "./constants";
 
 export default function getUrl() {
   let inputLang = window.location.search.split("=")[1];
+  inputLang = LANG_LIST.includes(inputLang) ? inputLang : "en";
+
   let userLang = inputLang || window.navigator.language.substr(0, 2);
 
   let url = new URL(window.location);
   url.searchParams.set("lang", userLang);
   history.replaceState(null, document.title, url);
-  return LANG_LIST.includes(userLang) ? userLang : "en";
+  return userLang;
 }
